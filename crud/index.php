8110dc86b568ff26e176982a777e0393b314e3a3
pythagoras58 <solomondanso58@gmail.com>
2023-06-04 22:48:23 +0800

    Index page design - 4

--- crud/index.php
+++ crud/index.php
@@ -13,60 +13,82 @@
         href="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.21/css/dataTables.bootstrap4.min.css"
         integrity="sha512-PT0RvABaDhDQugEbpNMwgYBCnGCiTZMh9yOzUsJHDgl/dMhD9yjHAwoumnUk3JydV3QTcIkNDuN40CJxik5+WQ=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert2/11.7.10/sweetalert2.css" integrity="sha512-otyZKuy0m2Vb2a/PvF29koS+TY1OB06a7YrE0Fvajv3L2crkLNNoxviT294+22mjLhWZ3z/Yb+vIy3ohjYP7Rg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert2/11.7.10/sweetalert2.css"
+        integrity="sha512-otyZKuy0m2Vb2a/PvF29koS+TY1OB06a7YrE0Fvajv3L2crkLNNoxviT294+22mjLhWZ3z/Yb+vIy3ohjYP7Rg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.1.0/css/v4-shims.min.css" integrity="sha512-p++g4gkFY8DBqLItjIfuKJPFvTPqcg2FzOns2BNaltwoCOrXMqRIOqgWqWEvuqsj/3aVdgoEo2Y7X6SomTfUPA==" crossorigin="anonymous" referrerpolicy="no-referrer" /> -->
 </head>
 
 <body>
 
-<nav class="navbar navbar-expand-md bg-dark navbar-dark">
-  <!-- Brand -->
-  <a class="navbar-brand" href="#"><i class="fab fa-wolf-pack-battalion"></i> Pythagorasweb</a>
+    <nav class="navbar navbar-expand-md bg-dark navbar-dark">
+        <!-- Brand -->
+        <a class="navbar-brand" href="#"><i class="fab fa-wolf-pack-battalion"></i> Pythagorasweb</a>
 
-  <!-- Toggler/collapsibe Button -->
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+        <!-- Toggler/collapsibe Button -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-  <!-- Navbar links -->
-  <div class="collapse navbar-collapse" id="collapsibleNavbar">
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item">
-        <a class="nav-link" href="#">Home</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">About</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Contact</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Service</a>
-      </li>
-    </ul>
-  </div>
-</nav>
+        <!-- Navbar links -->
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Service</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
 
-<div class="container">
-    <div class="row">
-        <div class="col-lg-12 mt-1">
-            <h4 class="text-center text-primary">MINI MANAGER</h4>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 mt-1">
+                <h4 class="text-center text-primary">MINI MANAGER</h4>
+            </div>
+        </div>
+
+
+        <div class="row">
+            <div class="col-lg-6">
+                <h4 class="text text-primary">All Data In DB!</h4>
+            </div>
+            <div class="col-lg-6">
+                <button type="button" class="btn btn-outline-primary m-1 float-right"><i
+                        class="fas fa-user-plus fa-lg"></i>&nbsp;&nbsp; Add User</button>
+                <a href="#" class="btn btn-outline-success m-1 float-right"><i
+                        class="fas fa-table fa-lg"></i>&nbsp;&nbsp; Export to Excel</a>
+            </div>
+        </div>
+
+        <hr class="my-1">
+
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="table-responsive" id="showUser">
+                    <table class="table table-striped table-sm table-bordered">
+                        <thead>
+                            <tr class="text-center">
+                                <th>ID</th>
+                                <th>FIRST NAME</th>
+                                <th>LAST NAME</th>
+                                <th>EMAIL</th>
+                                <th>ACTION</th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+            </div>
         </div>
     </div>
-
-
-    <div class="row">
-        <div class="col-lg-6">
-            <h4 class="text text-primary">All Data In DB!</h4>
-        </div>
-        <div class="col-lg-6">
-            <button type="button" class="btn btn-outline-primary m-1 float-right"><i class="fas fa-user-plus fa-lg"></i>&nbsp;&nbsp; Add User</button>
-            <a href="#" class="btn btn-outline-success m-1 float-right"><i class="fas fa-table fa-lg"></i>&nbsp;&nbsp; Export to Excel</a>
-        </div>
-    </div>
-
-    <hr class="my-1">
-</div>
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
         integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
@@ -81,7 +103,9 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.21/js/jquery.dataTables.min.js"
         integrity="sha512-BkpSL20WETFylMrcirBahHfSnY++H2O1W+UnEEO4yNIl+jI2+zowyoGJpbtk6bx97fBXf++WJHSSK2MV4ghPcg=="
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert2/11.7.10/sweetalert2.all.min.js" integrity="sha512-hqj3uRcg6pYUd2hJHI1k1qKyf8PA/YMWcEV9hTlD+mpnUKe2mpzS7Epp8FCQrGG632TEp2Bn194SN69Auykcdg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert2/11.7.10/sweetalert2.all.min.js"
+        integrity="sha512-hqj3uRcg6pYUd2hJHI1k1qKyf8PA/YMWcEV9hTlD+mpnUKe2mpzS7Epp8FCQrGG632TEp2Bn194SN69Auykcdg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
     <!-- <script type="text/javascript">
         $(document).ready(function () {
