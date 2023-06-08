f983461c7c6c59afb643472d3223636bd03b1587
pythagoras58 <solomondanso58@gmail.com>
2023-06-08 19:41:28 +0800

    Add User Modal design

--- crud/index.php
+++ crud/index.php
@@ -6,13 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Main Page</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="fontawesome/css/all.min.css" />
     <link rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.21/css/dataTables.bootstrap4.min.css"
         integrity="sha512-PT0RvABaDhDQugEbpNMwgYBCnGCiTZMh9yOzUsJHDgl/dMhD9yjHAwoumnUk3JydV3QTcIkNDuN40CJxik5+WQ=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="fontawesome/css/all.min.css" />
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert2/11.7.10/sweetalert2.css"
         integrity="sha512-otyZKuy0m2Vb2a/PvF29koS+TY1OB06a7YrE0Fvajv3L2crkLNNoxviT294+22mjLhWZ3z/Yb+vIy3ohjYP7Rg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -62,7 +63,7 @@
                 <h4 class="text text-primary">All Data In DB!</h4>
             </div>
             <div class="col-lg-6">
-                <button type="button" class="btn btn-outline-primary m-1 float-right"><i
+                <button type="button" class="btn btn-outline-primary m-1 float-right" data-toggle="modal" data-target="#myModal"><i
                         class="fas fa-user-plus fa-lg"></i>&nbsp;&nbsp; Add User</button>
                 <a href="#" class="btn btn-outline-success m-1 float-right"><i
                         class="fas fa-table fa-lg"></i>&nbsp;&nbsp; Export to Excel</a>
@@ -81,18 +82,66 @@
                                 <th>FIRST NAME</th>
                                 <th>LAST NAME</th>
                                 <th>EMAIL</th>
+                                <th>PHONE</th>
                                 <th>ACTION</th>
                             </tr>
                         </thead>
+
+                        <tbody>
+                            <?php for ($i = 0; $i < 100; $i++): ?>
+                                <tr class="text-center text-secondary">
+                                    <td>
+                                        <?php echo $i; ?>
+                                    </td>
+                                    <td>PAPI</td>
+                                    <td>DANSO</td>
+                                    <td>solomonda@gmail.com</td>
+                                    <td>13212321123</td>
+                                    <td>
+                                        <a href="#" title="View Details" class="text-success"><i
+                                                class="fas fa-info-circle fa-lg"></i></a>&nbsp;&nbsp;
+                                        <a href="#" title="Edit Details" class="text-primary"><i
+                                                class="fas fa-edit fa-lg"></i></a>&nbsp;&nbsp;
+                                        <a href="#" title="Edit Details" class="text-danger"><i
+                                                class="fas fa-trash-alt fa-lg"></i></a>
+                                    </td>
+                                </tr>
+
+                            <?php endfor; ?>
+                        </tbody>
                     </table>
                 </div>
             </div>
         </div>
     </div>
 
+
+    <!-- The Modal -->
+    <div class="modal fade" id="myModal">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+
+                <!-- Modal Header -->
+                <div class="modal-header">
+                    <h4 class="modal-title">Add New User</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+
+                <!-- Modal body -->
+                <div class="modal-body">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+    <!---END MODAL-->
+
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
         integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
         crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.21/js/jquery.dataTables.min.js"
+        integrity="sha512-BkpSL20WETFylMrcirBahHfSnY++H2O1W+UnEEO4yNIl+jI2+zowyoGJpbtk6bx97fBXf++WJHSSK2MV4ghPcg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
         integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
         crossorigin="anonymous"></script>
@@ -100,23 +149,16 @@
         integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
         crossorigin="anonymous"></script>
     <!-- <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script> -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.21/js/jquery.dataTables.min.js"
-        integrity="sha512-BkpSL20WETFylMrcirBahHfSnY++H2O1W+UnEEO4yNIl+jI2+zowyoGJpbtk6bx97fBXf++WJHSSK2MV4ghPcg=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert2/11.7.10/sweetalert2.all.min.js"
         integrity="sha512-hqj3uRcg6pYUd2hJHI1k1qKyf8PA/YMWcEV9hTlD+mpnUKe2mpzS7Epp8FCQrGG632TEp2Bn194SN69Auykcdg=="
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
-    <!-- <script type="text/javascript">
+    <script type="text/javascript">
         $(document).ready(function () {
-            Swal.fire({
-                title: 'Error!',
-                text: 'Do you want to continue',
-                icon: 'error',
-                confirmButtonText: 'Cool'
-            })
+            $('table').DataTable();
         });
-    </script> -->
+    </script>
 </body>
 
 </html>
